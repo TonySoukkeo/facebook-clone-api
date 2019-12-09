cddf7050989e470d3e4b36e2b8db428362d2be53
Tony <tonysoukkeodesign@gmail.com>
2019-12-09 00:32:51 -0700

    Modified headers

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Auth-Token, Origin"
+    "Content-Type, Authorization, Access-Control-Allow-Headers, X-Requested-With"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
